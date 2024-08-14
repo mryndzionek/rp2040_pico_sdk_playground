72399961669d953b6901b608b739e56c427911b4
Mariusz Ryndzionek <mryndzionek@gmail.com>
2024-08-14 14:04:09 +0200

    Added FastGRNN KWS demo app

---
+++ include/fbank.h
@@ -0,0 +1,15 @@
+#ifndef __FBANK_H__
+#define __FBANK_H__
+
+#define SAMPLE_LEN (16000UL)
+#define NUM_FRAMES (99UL)
+#define NUM_FILT (32UL)
+#define FRAME_STEP (160UL)
+#define FRAME_LEN (400UL)
+
+void fbank_init(void);
+void fbank(float *input, float (*output)[NUM_FILT], size_t size);
+void fbank_print_min_max(float input[NUM_FRAMES][NUM_FILT]);
+char const *const fbank_label_idx_to_str(size_t label);
+
+#endif // __FBANK_H__
